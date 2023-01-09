dbbf711a9ff7cc5422017c53b4308553809e1ade
MeDBeD1 <82283979+MeDBeD1@users.noreply.github.com>
2023-01-09 05:30:56 +0300

    Scenes Update

--- scenes/Misc/Weapon.gd
+++ scenes/Misc/Weapon.gd
@@ -13,3 +13,10 @@ func shoot():
 		var direction = (gun_position.global_position - gun_end.global_position).normalized()
 		GlobalSignals.emit_signal("bullet_fired", bullet_instance, gun_end.global_position, direction)
 		shoot_cooldown.start()
+
+func capture():
+	if shoot_cooldown.is_stopped() and Bullet != null:
+		var bullet_instance = Bullet.instance()
+		var direction = (gun_position.global_position - gun_end.global_position).normalized()
+		GlobalSignals.emit_signal("bullet_fired", bullet_instance, gun_end.global_position, direction)
+		shoot_cooldown.start()
